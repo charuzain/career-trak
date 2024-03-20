edaf545cad696250d65b6d4385d5f03fd5dd31ed
charuzain <charuabhizain@gmail.com>
2024-03-20 16:11:28 -0400

    Basic route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ]);
+
   return (
     <>
-      <h1>app</h1>
+      <RouterProvider router ={router} />
     </>
   );
 }
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div>HOME PAge</div>
+  )
+}
+
+export default Home
