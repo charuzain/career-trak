981b04171d6f5b61cec1ec797fd0a1b1c38b0ad1
charuzain <charuabhizain@gmail.com>
2024-03-24 00:31:54 -0400

    Error Page Added

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,14 +1,44 @@
-import pageNotFound from '../../assets/images/notFound.svg';
-import { Link } from 'react-router-dom';
+import pageNotFound from '../../assets/images/not-found.svg';
+import { Link, useRouteError } from 'react-router-dom';
+import './Error.scss';
 
 const Error = () => {
+  const errorResponse = useRouteError();
+  console.log(errorResponse.status);
+  if (errorResponse.status === 404) {
+    return (
+      <main className="error">
+        <div className="error__container">
+          <div className="error__img-container">
+            <img className="error__image" src={pageNotFound} alt="404 Error" />
+          </div>
+          <h1 className="error__title">Ohh!!Page Not Found</h1>
+          <h4 className="error__description">
+            Cant find the page you are looking for
+          </h4>
+          <Link to="/" className="error__link">
+            Back Home
+          </Link>
+        </div>
+      </main>
+    );
+  }
   return (
-    <main>
-      <img src={pageNotFound} alt="404 Error" />
-      <h1>Ohh!!Page Not Found</h1>
-      <h4>Can;t find the page you are looking for</h4>
-      <Link to="/">Back Home</Link>
-    </main>
+    <h1>Error</h1>
+    // <main className="error">
+    //   <div className="error__container">
+    //     <div className="error__img-container">
+    //       <img className="error__image" src={pageNotFound} alt="404 Error" />
+    //     </div>
+    //     <h1 className="error__title">Ohh!!Page Not Found</h1>
+    //     <h4 className="error__description">
+    //       Cant find the page you are looking for
+    //     </h4>
+    //     <Link to="/" className="error__link">
+    //       Back Home
+    //     </Link>
+    //   </div>
+    // </main>
   );
 };
 
