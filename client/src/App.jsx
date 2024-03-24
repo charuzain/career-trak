96d6481b12d2098f8d5cb860327efdb0bd8e7930
charuzain <charuabhizain@gmail.com>
2024-03-24 00:52:45 -0400

    Register route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Login from './pages/Login/Login';
 // import Landing from './pages/Landing/Landing';
 import Error from './pages/Error/Error';
 import Layout from './pages/Layout/Layout';
 import Landing from './pages/Landing/Landing';
+import Register from './pages/Register/Register';
 
 function App() {
   const router = createBrowserRouter([
@@ -19,6 +20,10 @@ function App() {
         {
           path: 'login',
           element: <Login />,
+        },
+        {
+          path: 'register',
+          element: <Register />,
         },
       ],
       errorElement: <Error />,
