5912c8a6444861d2e3505e8e693c75e2e5657aa1
charuzain <charuabhizain@gmail.com>
2024-03-31 02:20:33 -0400

    Add login page structure

--- client/src/components/FormRow/FormRow.jsx
+++ client/src/components/FormRow/FormRow.jsx
@@ -4,9 +4,17 @@ import './FormRow.scss';
 const FormRow = (props) => {
   console.log(props);
   return (
-    <div className='form-row'>
-      <label htmlFor={props.name} className='form-row__label'>{props.labelText}</label>
-      <input type={props.type} name={props.name} id={props.name} required className='form-row__input' />
+    <div className="form-row">
+      <label htmlFor={props.name} className="form-row__label">
+        {props.labelText}
+      </label>
+      <input
+        type={props.type}
+        name={props.name}
+        id={props.name}
+        required
+        className="form-row__input"
+      />
     </div>
   );
 };
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,28 @@
+import FormRow from '../../components/FormRow/FormRow';
+import logo from '../../assets/images/logo2.ico';
+import { Link } from 'react-router-dom';
+import './Login.scss'
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <main>
+      <div>
+        <div>
+          <img src={logo} alt="logo" />
+          <p>Trackify</p>
+        </div>
 
-export default Login
+        <form>
+          <h1>Login</h1>
+          <FormRow name="email" labelText="Email" type="email" />
+          <FormRow name="password" labelText="Password" type="password" />
+          <button type="submit">Login</button>
+          <button type="button">Explore the App</button>
+        </form>
+        <p>Not a member yet? <Link to ="/register">Register</Link></p>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
