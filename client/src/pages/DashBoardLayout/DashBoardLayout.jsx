bb923eb53af1b0312741c2ef5de031bad47c50d0
charuzain <charuabhizain@gmail.com>
2024-04-04 23:50:13 -0400

    Display SideBar componenet based on screen size

--- client/src/pages/DashBoardLayout/DashBoardLayout.jsx
+++ client/src/pages/DashBoardLayout/DashBoardLayout.jsx
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom"
-import NavBar from "../../components/NavBar/NavBar"
+import { Outlet } from 'react-router-dom';
+import './DashBoardLayout.scss';
+import NavBar from '../../components/NavBar/NavBar';
+import SmallSideBar from '../../components/SmallSideBar/SmallSideBar';
+import BigSideBar from '../../components/BigSideBar/BigSideBar';
 
 const DashBoardLayout = () => {
   return (
-    <>
-      <NavBar />
-      <Outlet />
-    </>
+    <div className="dashboard">
+      <SmallSideBar />
+      <BigSideBar />
+      <div className="dashboard__page">
+        <NavBar />
+        <Outlet />
+      </div>
+    </div>
   );
-}
+};
 
-export default DashBoardLayout
+export default DashBoardLayout;
